de6e46c28692c508a3f9d451f4999e296e9a889b
Jackson Prowell <jacksonprowell@proton.me>
2023-02-14 14:44:35 -0800

    Configs

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess({
+		postcss: true
+	}),
+
+	kit: adapter()
+};
+
+export default config;
---
+++ tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				blue: '#39ABEB',
+				red: '#FF6F6F',
+				yellow: '#FFFCB2',
+				blue_white: '#E3F9FF',
+				grey: '#818485',
+				paper: '#F5F5F5',
+				boldGrey: '#323438',
+				white: '#FFFFFF'
+			},
+			height: {
+				'100v': '100vh'
+			},
+			fontFamily: {
+				sans: ['Open Sans Medium', 'sans-serif'],
+				'sans-bold': ['Open Sans Bold', 'sans-serif'],
+				'sans-extra': ['Open Sans Extra Bold', 'sans-serif'],
+				'sans-semi': ['Open Sans Semi Bold', 'sans-serif'],
+				'sans-condensed': ['Open Sans Condensed', 'sans-serif'],
+				'serif-bold': ['PT Serif Bold', 'serif'],
+				sofia: ['Sofia Condensed', 'sans-serif']
+			},
+			screens: {
+				tablet: '640px',
+				laptop: '1024px',
+				'big-laptop': '1340px',
+				desktop: '1560px',
+				ultrawide: '1900px'
+			},
+			flex: {
+				2: '2',
+				3: '3'
+			}
+		}
+	},
+	plugins: []
+};
