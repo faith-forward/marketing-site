0c1b082607e2ece5fd3fdb62ca991d2674de09f3
Jackson Prowell <prowelljackson@gmail.com>
2023-05-12 19:09:42 -0700

    Built contact us, privacy policy, and tos pages

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	import Nav from '$lib/sections/nav.svelte';
+	import { faMedium, faTwitter, faReddit, faAppStore } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
+</script>
+
+<svelte:head>
+	<title>Contact Us | Personalized Christian Inspiration with Faith Forward</title>
+	<meta name="description" content="View our Contact information and reach out to us." />
+</svelte:head>
+
+<main class="flex flex-col bg-paper flex-1">
+	<div class="px-4 tablet:px-16 desktop:px-32">
+		<Nav />
+	</div>
+	<section
+		class="flex laptop:space-x-8 mt-8 flex-col items-center px-4 tablet:px-16 desktop:px-32 flex-1"
+	>
+		<h1 class="mb-4 text-4xl text-boldGrey font-sans-bold">Contact Us</h1>
+		<div
+			class="flex flex-col space-y-6 leading-relaxed px-4 laptop:w-3/5 tablet:w-4/5 w-full font-sans"
+		>
+			<h2 class="text-xl font-sans-semi">
+				We're here to listen, share, and support. Connect with us!
+			</h2>
+			<p>
+				At Faith Forward, we believe that every voice matters. Your questions, feedback, and
+				insights are invaluable to us. There are various ways you can reach out to us, either
+				through email or our social media platforms.
+			</p>
+			<h2 class="text-xl font-sans-semi">📬 Email Us</h2>
+			<p>
+				For general inquiries, suggestions, concerns, or technical support please feel free to send
+				us an email at <a href="mailto:hello@faithforward.app" class="text-blue"
+					>hello@faithforward.app</a
+				>. We strive to respond to all emails within 24 to 48 hours and to resolve all issues
+				promptly.
+			</p>
+			<h2 class="text-xl font-sans-semi">🌐 Connect with us on Social Media</h2>
+			<p>
+				Social media is more than just a communication tool for us. It is a vibrant community where
+				we share, discuss and engage with our members on a daily basis. Find us on your preferred
+				platform and join the conversation!
+			</p>
+			<ul class="ml-4 space-y-4">
+				<li>
+					<div class="flex items-start">
+						<a href="https://twitter.com/HiFaithForward" class="text-blue">
+							<Fa icon={faTwitter} class="mt-1.5 mr-4" />
+						</a>
+						<div>
+							<a href="https://twitter.com/HiFaithForward" class="text-blue font-sans-semi"
+								>Twitter</a
+							>: Get real-time updates and engage with us on important discussions. Our handle is
+							@HiFaithForward
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="flex items-start">
+						<a href="https://medium.com/@faithforward" class="text-blue">
+							<Fa icon={faMedium} class="mt-1.5 mr-4" />
+						</a>
+						<div>
+							<a href="https://medium.com/@faithforward" class="text-blue font-sans-semi">Medium</a
+							>: Explore our in-depth articles on faith, spirituality, and personal growth. Follow
+							us at @FaithForward
+						</div>
+					</div>
+				</li>
+			</ul>
+			<p>
+				Remember, we are here to support you on your spiritual journey. So don't hesitate to reach
+				out. We can't wait to hear from you!
+			</p>
+			<h2 class="text-xl font-sans-semi">🙏 Stay Blessed, Stay Connected</h2>
+		</div>
+	</section>
+</main>
+
+<style>
+	h2 {
+		margin-top: 54px !important;
+	}
+</style>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,144 @@
+<script>
+	import Nav from '$lib/sections/nav.svelte';
+</script>
+
+<svelte:head>
+	<title>Terms of Service | Personalized Christian Inspiration with Faith Forward</title>
+	<meta name="description" content="Read our Terms of Service." />
+</svelte:head>
+
+<main class="flex flex-col bg-paper flex-1">
+	<div class="px-4 tablet:px-16 desktop:px-32">
+		<Nav />
+	</div>
+	<section
+		class="flex laptop:space-x-8 mt-8 flex-col items-center px-4 tablet:px-16 desktop:px-32 flex-1 font-sans"
+	>
+		<h1 class="mb-4 text-4xl text-boldGrey font-sans-bold">Privacy Policy</h1>
+		<div class="flex flex-col space-y-6 leading-relaxed px-4 laptop:w-3/5 tablet:w-4/5 w-full">
+			<h2 class="text-xl font-sans-semi">Privacy Policy</h2>
+			<p>
+				Lil Bro Co built the Faith Forward app as a Freemium app. This SERVICE is provided by Lil
+				Bro Co at no cost and is intended for use as is.
+			</p>
+			<p>
+				This page is used to inform visitors regarding our policies with the collection, use, and
+				disclosure of Personal Information if anyone decided to use our Service.
+			</p>
+			<p>
+				If you choose to use our Service, then you agree to the collection and use of information in
+				relation to this policy. The Personal Information that we collect is used for providing and
+				improving the Service. We will not use or share your information with anyone except as
+				described in this Privacy Policy.
+			</p>
+			<p>
+				The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
+				which are accessible at Faith Forward unless otherwise defined in this Privacy Policy.
+			</p>
+			<h2 class="text-xl font-sans-semi mt-24">Information Collection and Use</h2>
+			<p>
+				For a better experience, while using our Service, we may require you to provide us with
+				certain personally identifiable information, including but not limited to email address. The
+				information that we request will be retained by us and used as described in this privacy
+				policy.
+			</p>
+
+			<p>
+				The app does use third-party services that may collect information used to identify you.
+			</p>
+			<p>
+				Link to the privacy policy of third-party service providers used by the app: <a
+					href="https://expo.io/privacy"
+					class="text-blue">Expo</a
+				>
+			</p>
+
+			<h2 class="text-xl font-sans-semi">Log Data</h2>
+			<p>
+				We want to inform you that whenever you use our Service, in a case of an error in the app we
+				collect data and information (through third-party products) on your phone called Log Data.
+				This Log Data may include information such as your device Internet Protocol (“IP”) address,
+				device name, operating system version, the configuration of the app when utilizing our
+				Service, the time and date of your use of the Service, and other statistics.
+			</p>
+			<h2 class="text-xl font-sans-semi">Cookies</h2>
+			<p>
+				Cookies are files with a small amount of data that are commonly used as anonymous unique
+				identifiers. These are sent to your browser from the websites that you visit and are stored
+				on your device's internal memory.
+			</p>
+			<p>
+				This Service does not use these “cookies” explicitly. However, the app may use third-party
+				code and libraries that use “cookies” to collect information and improve their services. You
+				have the option to either accept or refuse these cookies and know when a cookie is being
+				sent to your device. If you choose to refuse our cookies, you may not be able to use some
+				portions of this Service.
+			</p>
+			<h2 class="text-xl font-sans-semi">Service Providers</h2>
+			<p>We may employ third-party companies and individuals due to the following reasons:</p>
+			<ul class="list-disc ml-8">
+				<li>To facilitate our Service;</li>
+				<li>To provide the Service on our behalf;</li>
+				<li>To perform Service-related services; or</li>
+				<li>To assist us in analyzing how our Service is used.</li>
+			</ul>
+			<p>
+				We want to inform users of this Service that these third parties have access to their
+				Personal Information. The reason is to perform the tasks assigned to them on our behalf.
+				However, they are obligated not to disclose or use the information for any other purpose.
+			</p>
+			<h2 class="text-xl font-sans-semi">Security</h2>
+			<p>
+				We value your trust in providing us your Personal Information, thus we are striving to use
+				commercially acceptable means of protecting it. But remember that no method of transmission
+				over the internet, or method of electronic storage is 100% secure and reliable, and we
+				cannot guarantee its absolute security.
+			</p>
+			<h2 class="text-xl font-sans-semi">Links to Other Sites</h2>
+			<p>
+				This Service may contain links to other sites. If you click on a third-party link, you will
+				be directed to that site. Note that these external sites are not operated by us. Therefore,
+				we strongly advise you to review the Privacy Policy of these websites. We have no control
+				over and assume no responsibility for the content, privacy policies, or practices of any
+				third-party sites or services.
+			</p>
+			<h2 class="text-xl font-sans-semi">Children’s Privacy</h2>
+			<p>
+				These Services do not address anyone under the age of 13. We do not knowingly collect
+				personally identifiable information from children under 13 years of age. In the case we
+				discover that a child under 13 has provided us with personal information, we immediately
+				delete this from our servers. If you are a parent or guardian and you are aware that your
+				child has provided us with personal information, please contact us so that we will be able
+				to do the necessary actions.
+			</p>
+			<h2 class="text-xl font-sans-semi">Changes to This Privacy Policy</h2>
+			<p>
+				We may update our Privacy Policy from time to time. Thus, you are advised to review this
+				page periodically for any changes. We will notify you of any changes by posting the new
+				Privacy Policy on this page.
+			</p>
+			<p>This policy is effective as of 2023-01-05</p>
+			<h2 class="text-xl font-sans-semi">Contact Us</h2>
+			<p>
+				If you have any questions or suggestions about our Privacy Policy, do not hesitate to
+				contact us at <a href="mailto:hello@faithforward.app" class="text-blue"
+					>hello@faithforward.app</a
+				>.
+			</p>
+			<p>
+				This privacy policy page was created at
+				<a href="https://privacypolicytemplate.net" class="text-blue">privacypolicytemplate.net</a>
+				and modified/generated by
+				<a href="https://app-privacy-policy-generator.nisrulz.com/" class="text-blue"
+					>App Privacy Policy Generator</a
+				>
+			</p>
+		</div>
+	</section>
+</main>
+
+<style>
+	h2 {
+		margin-top: 54px !important;
+	}
+</style>
