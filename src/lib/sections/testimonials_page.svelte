407dcad885ab66eebdc40d94cbb089163630d800
Jackson Prowell <prowelljackson@gmail.com>
2023-05-14 00:12:42 -0700

    Added new testimonials

--- src/lib/sections/testimonials_page.svelte
+++ src/lib/sections/testimonials_page.svelte
@@ -12,25 +12,41 @@
 		</p>
 	</div>
 	<div
-		class="flex flex-col laptop:flex-row relative top-0 laptop:top-[-64px] laptop:justify-between mt-8 laptop:mt-32 items-center laptop:items-start"
+		class="flex flex-col laptop:flex-row relative top-0 laptop:top-[-64px] laptop:justify-between mt-8 laptop:mt-32 items-center laptop:items-start big-laptop:space-x-32 tablet:space-x-4"
 	>
 		<img
 			use:lazyLoad={'/images/Testimonial 1.webp'}
 			alt={`M. Jodan said this about Faith Forward: "Love it! Straight to the point. I would highly recommend!"`}
-			class="w-3/5 laptop:w-2/5 test"
+			class="w-3/5 laptop:w-[500px] test"
 		/>
 		<img
 			use:lazyLoad={'/images/Testimonial 2.webp'}
 			alt={`Mssmee said this about Faith Forward: "I really like how easit it is to get specific information pertaining to whatever emotion I'm feeling in the moment."`}
 			id="testimonial2"
-			class="w-3/5 laptop:w-2/5 mt-8 laptop:mt-[124px]"
+			class="w-3/5 laptop:w-[500px] mt-8 laptop:mt-[240px]"
+		/>
+	</div>
+	<div
+		class="flex flex-col laptop:flex-row relative top-0 laptop:top-[-64px] laptop:justify-between mt-8 laptop:mt-32 items-center laptop:items-start big-laptop:space-x-64 tablet:space-x-4"
+	>
+		<img
+			use:lazyLoad={'/images/Testimonial 4.webp'}
+			alt={`N.J. had this to say about Faith Forward: "If church was more like Faith Forward, I'd go to church."`}
+			id="testimonial3"
+			class="w-3/5 laptop:w-[500px] mt-8 laptop:mt-[-124px]"
+		/>
+		<img
+			use:lazyLoad={'/images/Testimonial 3.webp'}
+			alt={`N.J. had this to say about Faith Forward: "If church was more like Faith Forward, I'd go to church."`}
+			id="testimonial3"
+			class="w-3/5 laptop:w-[500px] mt-8 "
 		/>
 	</div>
 	<img
-		use:lazyLoad={'/images/Testimonial 3.webp'}
+		use:lazyLoad={'/images/Testimonial 5.webp'}
 		alt={`N.J. had this to say about Faith Forward: "If church was more like Faith Forward, I'd go to church."`}
 		id="testimonial3"
-		class="w-3/5 laptop:w-2/5 mt-8 laptop:mt-[-64px]"
+		class="w-3/5 laptop:w-[500px] mt-8 laptop:mt-[-32px]"
 	/>
 </div>
 
