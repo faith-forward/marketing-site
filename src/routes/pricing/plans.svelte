6fbe22ea43260d3aa2d155cd037feb630e1f3cbb
phaedrus <phaedrus.raznikov@pm.me>
2023-11-16 08:40:01 -0600

    style(pricing): add action buttons

--- src/routes/pricing/plans.svelte
+++ src/routes/pricing/plans.svelte
@@ -78,36 +78,3 @@
 		<PricingCard {...plan} />
 	{/each}
 </div>
-
-<!-- <h2 class="text-2xl text-boldGrey font-sans-bold self-start mt-16">For Individuals</h2> -->
-<!-- <p class="font-sans text-grey self-start"> -->
-<!-- 	Explore scripture, strengthen your faith, and connect more deeply with your church. Choose the -->
-<!-- 	plan that best supports your spiritual journey. -->
-<!-- </p> -->
-<!-- <div class="flex flex-col tablet:flex-row tablet:space-x-8 w-full mt-8"> -->
-<!-- 	{#each individualPlans as plan (plan.level)} -->
-<!-- 		<PricingCard {...plan} planType="individual" /> -->
-<!-- 	{/each} -->
-<!-- </div> -->
-<!---->
-<!-- <h2 class="text-2xl text-boldGrey font-sans-bold self-start mt-16">For Churches</h2> -->
-<!-- <p class="font-sans text-grey self-start"> -->
-<!-- 	Enhance your administrative capabilities and elevate your congregation's engagement. Discover the -->
-<!-- 	plan that aligns with your church's mission and needs. -->
-<!-- </p> -->
-<!-- <div class="flex flex-col tablet:flex-row tablet:space-x-8 w-full mt-8"> -->
-<!-- 	{#each churchPlans as plan (plan.level)} -->
-<!-- 		<PricingCard {...plan} planType="church" /> -->
-<!-- 	{/each} -->
-<!-- </div> -->
-<!---->
-<!-- <h2 class="text-2xl text-boldGrey font-sans-bold self-start mt-16">For Enterprise</h2> -->
-<!-- <p class="font-sans text-grey self-start"> -->
-<!-- 	We offer custom integration services for organizations looking to leverage our AI-powered platform -->
-<!-- 	to enhance their own products and services. -->
-<!-- </p> -->
-<!-- <div class="flex flex-col tablet:flex-row tablet:space-x-8 w-full mt-8"> -->
-<!-- 	{#each enterprisePlans as plan (plan.level)} -->
-<!-- 		<PricingCard {...plan} planType="enterprise" /> -->
-<!-- 	{/each} -->
-<!-- </div> -->
--- src/routes/pricing/pricing_card.svelte
+++ src/routes/pricing/pricing_card.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import initiateDownload from '$lib/pixel/initiateDownload';
 	import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 	import Fa from 'svelte-fa';
 
@@ -10,7 +11,7 @@
 </script>
 
 <div
-	class={`flex flex-col p-6 ${
+	class={`flex flex-col justify-between p-6 ${
 		planType === 'church'
 			? 'bg-churchBackground'
 			: planType === 'enterprise'
@@ -18,39 +19,74 @@
 			: 'bg-white'
 	} mb-8 tablet:mb-0 rounded-lg shadow-lg flex-1`}
 >
-	<h2
-		class="text-xs font-sans-bold uppercase"
-		class:text-blue={planType === 'individual'}
-		class:text-[#a8dfff]={planType !== 'individual'}
-	>
-		{level}
-	</h2>
-	{#if subtitle}
+	<div>
+		<h2
+			class="text-xs font-sans-bold uppercase"
+			class:text-blue={planType === 'individual'}
+			class:text-[#a8dfff]={planType !== 'individual'}
+		>
+			{level}
+		</h2>
+		{#if subtitle}
+			<p
+				class="text-xs font-sans mt-2"
+				class:text-grey={planType === 'individual'}
+				class:text-white={planType !== 'individual'}
+			>
+				{subtitle}
+			</p>
+		{/if}
 		<p
-			class="text-xs font-sans mt-2"
+			class="text-2xl font-sans-bold mt-2"
+			class:text-boldGrey={planType === 'individual'}
+			class:text-white={planType !== 'individual'}
+		>
+			{price}
+		</p>
+		<ul
+			class="flex flex-col space-y-2 mt-4"
 			class:text-grey={planType === 'individual'}
 			class:text-white={planType !== 'individual'}
 		>
-			{subtitle}
-		</p>
+			{#each features as feature (feature)}
+				<li class="flex items-center">
+					<Fa icon={faCheckCircle} class="mr-2" />
+					<p class="text-sm">{feature}</p>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	{#if planType === 'individual'}
+		<a
+			href="https://apps.apple.com/us/app/faith-forward/id1661516199"
+			on:click={() => initiateDownload('/')}
+		>
+			<button
+				class="bg-blue text-white font-sans-bold uppercase text-sm rounded-lg py-2 mt-4 w-full"
+			>
+				Download
+			</button>
+		</a>
 	{/if}
-	<p
-		class="text-2xl font-sans-bold mt-2"
-		class:text-boldGrey={planType === 'individual'}
-		class:text-white={planType !== 'individual'}
-	>
-		{price}
-	</p>
-	<ul
-		class="flex flex-col space-y-2 mt-4"
-		class:text-grey={planType === 'individual'}
-		class:text-white={planType !== 'individual'}
-	>
-		{#each features as feature (feature)}
-			<li class="flex items-center">
-				<Fa icon={faCheckCircle} class="mr-2" />
-				<p class="text-sm">{feature}</p>
-			</li>
-		{/each}
-	</ul>
+
+	{#if planType === 'church'}
+		<a href="https://admin.faithforward.app/signup">
+			<button
+				class="bg-white text-blue font-sans-bold uppercase text-sm rounded-lg py-2 mt-4 w-full"
+			>
+				Sign Up
+			</button>
+		</a>
+	{/if}
+
+	{#if planType === 'enterprise'}
+		<a href="mailto:hello@faithforward.app">
+			<button
+				class="bg-white text-enterpriseBlue font-sans-bold uppercase text-sm rounded-lg py-2 mt-4 w-full"
+			>
+				Contact Us
+			</button>
+		</a>
+	{/if}
 </div>
